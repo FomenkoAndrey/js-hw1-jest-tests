333a36eb5465c22f136c34d258ce4cdcf165234d
Alecto <fomenkoandrey1978@gmail.com>
2024-03-26 08:47:56 +0200

    додано тест 3

--- src/main.js
+++ src/main.js
@@ -5,14 +5,14 @@ console.log('#1. JavaScript homework example file')
  *
  * Створіть змінні зі значеннями.
  */
-var myNum = 10 // variable name: myNum, value: 10
-var myStr = 'some string' // variable name: myStr, value: 'some string'
-var myBool = true // variable name: myBool, value: true
-var myArr = [1, 2, 3, 4, 5] // variable name: myArr, value: 1, 2, 3, 4, 5
+var myNum = 10 // змінна myNum зі значенням 10
+var myStr = 'some string' // змінна myStr зі значенням 'some string'
+var myBool = true // змінна myBool зі значенням true
+var myArr = [1, 2, 3, 4, 5] // змінна myArr зі значенням масиву [1, 2, 3, 4, 5]
 var myObj = {
   first: 'First Name',
   last: 'Last Name'
-} // name of the variable myObj, value: first: 'First Name', last: 'Last Name'
+} // змінна myObj зі значенням об'єкту {first: 'First Name', last: 'Last Name'}
 
 console.log('myNum: ', myNum)
 console.log('myStr:', myStr)
@@ -26,5 +26,16 @@ console.log('myObj:', myObj)
  * Відформатуйте ціле число, яке зберігається в змінній myNum, щоб отримати результат з 2 знаками після коми.
  * Результат збережіть у змінній decimal2.
  */
-var decimal2 = myNum.toFixed(2)
+var decimal2 = myNum.toFixed(2) // Форматування числа myNum збереженого в змінній myNum з 2 знаками після коми
 console.log('decimal2:', decimal2)
+
+
+/*
+ * #3
+ *
+ * Створіть змінну myBigInt і запишіть в неї число 123n (BigInt).
+ * Потім збільште його на 1 та запищіть в цю ж саму змінну.
+ */
+var myBigInt = 123n // Створення змінної myBigInt та присвоєння їй значення BigInt 123n
+myBigInt = myBigInt + 1n // Збільшення значення myBigInt на 1n
+console.log('myBigInt:', myBigInt)
